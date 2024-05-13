2008c2cdcd23ffef12cad63a8542e70e77066109
samiaBoug <bougtoubsamia@gmail.com>
2024-05-13 13:25:57 +0100

    f

--- view/user.php
+++ view/user.php
@@ -19,7 +19,12 @@ include 'include/header.php';
 </header>
 <main>
 <h1>Bilan Carbone </h1>
-
+<h3>Total de l'empreinte carbone annuelle</h3> <span></span><h4>en tonne de CO2 par année</h4>
+<h3>moyenne nationale annuelle </h3> <span></span><h4>en tonne de CO2 par année</h4>
+<h3>moyenne mondiale annuelle </h3><span>4,9</span><h4>en tonne de CO2 par année</h4>
+<h2>Bilan de moi </h2>
+<h2>Bilan mentuel</h2>
+<h2>Bilan anuel</h2>
 </main>
 <?php
 include 'include/footer.php';
